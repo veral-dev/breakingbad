a81e3ed89bb729a730eb592255462918cc0858e2
davidvrl <veral.david@gmail.com>
2020-03-25 17:54:37 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Text from './components/text'
+
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+background:-webkit-linear-gradient(top left, #007d35 0%,#007d35 40%, #0f574e 100% );
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #fff;
+margin-top: 1rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+border-radius: 5px;
+transition: background-size .8s ease;
+
+
+:hover{
+  cursor: pointer;
+  background-size: 400px
+}
+
+`
+
+const Container = styled.div`
+display: flex;
+align-items: center;
+padding-top: 5rem;
+flex-direction: column;
+`
+
+
 
 function App() {
+
+  const [text, setText] = useState({})
+
+  const apiConsult = async () => {
+    // fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    //   .then(data => data.json())
+    //   .then(result => console.log(result))
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const text = await api.json()
+    setText(text[0])
+  }
+
+  //Cargar una frase
+  useEffect(() => {
+    apiConsult()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Text
+        text={text}
+      />
+      <Button onClick={apiConsult}>Obtener frase</Button>
+    </Container>
   );
 }
 
